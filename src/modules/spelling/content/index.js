9f306ca4723b21d6d990a84d67e98f1c561e8c3d
JamesJennings <gccsatx@gmail.com>
2026-04-19 21:51:54 -0500

    Spelling Expansion: K-12 full coverage + 50 words added per grade 1-8

--- src/modules/spelling/content/index.js
+++ src/modules/spelling/content/index.js
@@ -5,24 +5,34 @@
  * to the nearest lower grade if an exact match isn't found.
  */
 
-import grade1 from "./grade1";
-import grade2 from "./grade2";
-import grade3 from "./grade3";
-import grade4 from "./grade4";
-import grade5 from "./grade5";
-import grade6 from "./grade6";
-import grade7 from "./grade7";
-import grade8 from "./grade8";
+import gradeK  from "./gradeK";
+import grade1  from "./grade1";
+import grade2  from "./grade2";
+import grade3  from "./grade3";
+import grade4  from "./grade4";
+import grade5  from "./grade5";
+import grade6  from "./grade6";
+import grade7  from "./grade7";
+import grade8  from "./grade8";
+import grade9  from "./grade9";
+import grade10 from "./grade10";
+import grade11 from "./grade11";
+import grade12 from "./grade12";
 
 const GRADE_CONTENT = {
-  1: grade1,
-  2: grade2,
-  3: grade3,
-  4: grade4,
-  5: grade5,
-  6: grade6,
-  7: grade7,
-  8: grade8,
+  0:  gradeK,
+  1:  grade1,
+  2:  grade2,
+  3:  grade3,
+  4:  grade4,
+  5:  grade5,
+  6:  grade6,
+  7:  grade7,
+  8:  grade8,
+  9:  grade9,
+  10: grade10,
+  11: grade11,
+  12: grade12,
 };
 
 /**
